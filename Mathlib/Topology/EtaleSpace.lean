57db7890798c2c0701d550f1acfb15f60ddc6af7
Junyan Xu <junyanxu.math@gmail.com>
2025-07-16 11:29:15 +0200

    shake

--- Mathlib/Topology/EtaleSpace.lean
+++ Mathlib/Topology/EtaleSpace.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Junyan Xu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Junyan Xu
 -/
-import Mathlib.Topology.Homotopy.Lifting
+import Mathlib.Topology.Covering
 import Mathlib.Topology.IsLocalHomeomorph
 import Mathlib.Topology.Sheaves.LocalPredicate
 
